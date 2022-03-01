d79cffc771f48cd92772265a2abf03a1971da987
Lucadoge <lucampayet@gmail.com>
2022-03-01 15:59:13 +0100

    added secret to .env

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
 
 import discord
-import discord.ext.commands
-import discord.ext.tasks
+from discord.ext import commands
+from discord.ext import tasks
 
 import asyncio
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 i_prefix = '!' # change this
 i_intents = Intents.all()
 
 Client = commands.Bot(command_prefix=i_prefix, intents=i_intents)
 
+# bot code
 
-
-
-
-
-
-
-
-s_token = os.environ.get('BOT_TOKEN') # set an env variable -> export BOT_TOKEN='super sekrit token' <- or somethin
+s_token = os.getenv('BOT_TOKEN') # hidden in .env file (git-ignored)
 
 await Client.login(token=s_token, bot=True)
 await Client.connect(reconnect=True)
