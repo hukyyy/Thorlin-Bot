9e272e0db53f187c6baeb0732f9fe143f75a2d65
Lucadoge <lucampayet@gmail.com>
2022-03-01 11:13:08 +0100

    initial commit wooo

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import discord
+import discord.ext.commands
+import discord.ext.tasks
+
+import asyncio
+import os
+
+i_prefix = '!' # change this
+i_intents = Intents.all()
+
+Client = commands.Bot(command_prefix=i_prefix, intents=i_intents)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+s_token = os.environ.get('BOT_TOKEN') # set an env variable -> export BOT_TOKEN='super sekrit token' <- or something))
