893229bae84ad554f459c1df3f8da0d74691fba4
Lucadoge <lucampayet@gmail.com>
2022-03-04 10:38:18 +0100

    fixed-ish but my computer is dying itd on 2% :cccc

--- main.py
+++ main.py
@@ -15,24 +15,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-reddit = praw.Reddit(
-    client_id = os.getenv('REDDIT_CLIENT_ID'),
-    client_secret = os.getenv('REDDIT_CLIENT_SECRET'),
-    refresh_token = os.getenv('REDDIT_REFRESH_TOKEN'),
-    user_agent = os.getenv('REDDIT_USER_AGENT'),
-)
-
-reddit.read_only = True
-
 i_prefix = '!' # change this
 i_intents = discord.Intents.all()
 
+_im = ['im', 'i\'m', 'i am']
+annoylist = set()
+
 Client = commands.Bot(command_prefix=i_prefix, intents=i_intents)
+
+reddit = praw.Reddit(
+client_id = os.getenv('REDDIT_CLIENT_ID'),
+client_secret = os.getenv('REDDIT_CLIENT_SECRET'),
+refresh_token = os.getenv('REDDIT_REFRESH_TOKEN'),
+user_agent = os.getenv('REDDIT_USER_AGENT'),
+)
+reddit.read_only = True
+
 
 @Client.command()
 async def hi(ctx):
     await ctx.message.channel.send(f"Hello, {ctx.message.author.name}!")
-
 
 # Imporvements necessary :
 # - Filter image posts
@@ -46,7 +48,7 @@ async def adventurecat(ctx):
     #
     # if galleries:
     #     s = requests.Session()
-    #     s.headers.update({'User-Agent': 'sex'})
+    #     s.headers.update({'User-Agent': 'Thorlin-Bot'})
     #     imglinks = [re.search(r'https:\/\/preview.redd.it\/[a-z0-9]{13}\.jpg', s.get(galleries[i][2]).text).group() for i in range(len(galleries))]
     #     images += imglinks
 
@@ -66,5 +68,27 @@ async def showerthought(ctx):
 
     await ctx.message.channel.send(embed = embed)
 
+@Client.command()
+async def annoy(ctx, op='start'):
+    f'''Annoys a pinged user: {i_prefix}annoy (start|stop) \@user'''
+    global annoylist
+    if (op.lower() == 'start'):
+        annoylist.add(ctx.message.mentions[0])
+    elif (op.lower() == 'stop'):
+        annoylist.remove(ctx.message.mentions[0])
+    else:
+        ctx.send('Didn\'t quite get that :b')
+
+@Client.listen('on_message')
+async def _annoy(message):
+    global annoylist
+    print(message.content, message.author, *annoylist)
+    if message.author in annoylist:
+        if any(list := (w in message.content.lower() for w in _im)):
+            present = [a * b for a, b in zip(list, _im)]
+            msg = message.content.split(' ')[(message.content.split(' ').index(present[0]))]
+            message.channel.send(f'''Hi {' '.join(msg)}, I\'m dad.''')
+        else:
+            message.channel.send(''.join(random.choice((str.upper, str.lower))(c) for c in message.content.lower()))
 
 Client.run(os.getenv('BOT_TOKEN'))
