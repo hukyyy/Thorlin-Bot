fa3cd4747d731e48036acc726bedb32f8fc1176e
Lucadoge <lucampayet@gmail.com>
2022-03-04 12:39:41 +0100

    perfection

--- main.py
+++ main.py
@@ -34,16 +34,20 @@ reddit.read_only = True
 
 @Client.command()
 async def hi(ctx):
+
     await ctx.message.channel.send(f"Hello, {ctx.message.author.name}!")
 
-# Imporvements necessary :
+
+# Improvements necessary :
 # - Filter image posts
 # - Handle image galleries
 @Client.command()
 async def adventurecat(ctx):
+
     posts = [[post.title, post.author, post.url, post.permalink] for post in reddit.subreddit("adventurecats").hot(limit = 50)]
 
     images = list(filter(lambda p: 'i.redd.it' in p[2], posts))
+
     # galleries = list(filter(lambda p: 'gallery' in p[2], posts))
     #
     # if galleries:
@@ -54,8 +58,10 @@ async def adventurecat(ctx):
 
     await ctx.message.channel.send(random.choice(images[2]))
 
+
 @Client.command()
 async def showerthought(ctx):
+
     thoughts = [[thought.title, thought.author, thought.permalink] for thought in reddit.subreddit("showerthoughts").hot(limit = 50)]
 
     chosen = random.choice(thoughts)
@@ -68,28 +74,39 @@ async def showerthought(ctx):
 
     await ctx.message.channel.send(embed = embed)
 
-@Client.command()
+
+@Client.command(usage='(start|stop) @user')
 async def annoy(ctx, op='start'):
-    f'''Annoys a pinged user: {i_prefix}annoy (start|stop) \@user'''
+
     global annoylist
+
     if (op.lower() == 'start'):
         annoylist.add(ctx.message.mentions[0])
+
     elif (op.lower() == 'stop'):
         annoylist.remove(ctx.message.mentions[0])
+
     else:
         await ctx.send('Didn\'t quite get that :b')
 
+
 @Client.listen('on_message')
 async def _annoy(message):
+
     global annoylist
-    if not message.content.startswith('!'):
-        if message.author in annoylist:
-            if any(list := [w in message.content.lower() for w in _im]):
-                present = [int(a) * b for a, b in zip(list, _im)]
-                present[:] = [x for x in present if x != '']
-                msg = message.content.split(' ')[(message.content.split(' ').index(present[0])):]
-                await message.channel.send(f'''Hi {' '.join(msg[1:])}, I\'m dad.''')
-            else:
-                await message.channel.send(''.join(random.choice((str.upper, str.lower))(c) for c in message.content.lower()))
+
+    if (not message.content.startswith('!') and message.author in annoylist):
+
+        if any(list := [w in message.content.lower() for w in _im]):
+
+            present = [int(a) * b for a, b in zip(list, _im)]
+            present[:] = [x for x in present if x != '']
+
+            msg = message.content.split(' ')[(message.content.split(' ').index(present[0])):]
+            await message.channel.send(f'''Hi {' '.join(msg[1:])}, I\'m dad.''')
+
+        else:
+            await message.channel.send(''.join(random.choice((str.upper, str.lower))(c) for c in message.content.lower()))
+
 
 Client.run(os.getenv('BOT_TOKEN'))
