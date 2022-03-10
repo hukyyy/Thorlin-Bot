ff879cf91f1768789784e8171d1536afdf754b44
Lucadoge <lucampayet@gmail.com>
2022-03-10 23:00:35 +0100

    old lib is shit, moving to ytdl

--- cogs/music.py
+++ cogs/music.py
@@ -1,35 +1,64 @@
 import discord
 from discord.ext import commands
 import ffmpeg
-from pytube import YouTube, Search, Playlist
+import youtube_dl
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
 
 class music(commands.Cog):
     def __init__(self, client):
         self.Client = client
         self.queue = []
 
-    async def get_stream(self, link: str, abr: int):
-
-        yt = YouTube(link)
-
-        audiostreams = yt.streams.filter(only_audio=True)
-        bitrates = [int(s.abr.replace('kbps', '')) for s in audiostreams]
-
-        closestbr = min(bitrates, key=lambda x:abs(x-abr))
-
-        PCMStream = discord.FFmpegPCMAudio(
-        source = yt.streams.filter(only_audio=True, abr=f'{closestbr}kbps')[0].url,
-        options = f'-b:a {closestbr}K')
-
-        return yt, PCMStream
-
 
     @commands.command()
     async def join(self, ctx):
-
         if ctx.voice_client is not None:
             return await ctx.voice_client.move_to(channel)
-
         await ctx.message.author.channel.connect()
 
 
@@ -40,73 +69,13 @@ class music(commands.Cog):
 
 
     @commands.command(aliases=['p'])
-    async def play(self, ctx, *, link: str):
+    async def play(self, ctx, *, url: str):
 
-        if not 'www.' in link:
-            ctx.send('Not a link! Please use !search or !s')
-            return
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.Client.loop, stream=True)
+            ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
-        yt, PCMStream = await self.get_stream(link, ctx.voice_client.channel.bitrate)
-
-        title = yt.title
-        thumbnail_url = yt.thumbnail_url
-
-        ctx.voice_client.play(PCMStream, after=lambda e: print('Player error: %s' % e) if e else None)
-
-        embed = discord.Embed(title = f'Playing {yt.title}!', color=0xc4302b)
-        embed.set_thumbnail(url = thumbnail_url)
-        await ctx.send(embed=embed, delete_after=30)
-
-
-    @commands.command(aliases=['s'])
-    async def search(self, ctx, *, query):
-        s = Search(query)
-        # filter(s.results, lambda s: s.length != 0)
-
-        activesearch = []
-
-        embed=discord.Embed(title=f'Search reults for: {query}', url=f'''https://www.youtube.com/results?search_query={query.replace(' ', '+')}''', color=0xc4302b)
-        embed.set_thumbnail(url=s.results[0].thumbnail_url)
-        embed.add_field(name=s.results[0].title, value=s.results[0].author, inline=False)
-        m = await ctx.send(embed=embed, delete_after=30)
-        await m.add_reaction('✅')
-
-        activesearch.append(m)
-
-        for i in s.results[1:5]:
-            embed=discord.Embed(color=0xc4302b)
-            embed.set_thumbnail(url=i.thumbnail_url)
-            embed.add_field(name=i.title, value=i.author, inline=False)
-            m = await ctx.send(embed=embed, delete_after=30)
-            await m.add_reaction('✅')
-            activesearch.append(m)
-
-        reacted = await self.Client.wait_for(
-        event = 'reaction_add',
-        check = lambda r, u: u == ctx.author and r.message in activesearch,
-        timeout = 30
-        )
-        song = activesearch.index(reacted[0].message)
-
-        yt = s.results[song]
-
-        audiostreams = yt.streams.filter(only_audio=True)
-        bitrates = [int(s.abr.replace('kbps', '')) for s in audiostreams]
-
-        closestbr = min(bitrates, key=lambda x:abs(x-ctx.voice_client.channel.bitrate))
-
-        PCMStream = discord.FFmpegPCMAudio(
-        source = yt.streams.filter(only_audio=True, abr=f'{closestbr}kbps')[0].url,
-        options = f'-b:a {closestbr}K')
-
-        title = yt.title
-        thumbnail_url = yt.thumbnail_url
-
-        ctx.voice_client.play(PCMStream, after=lambda e: print('Player error: %s' % e) if e else None)
-
-        embed = discord.Embed(title = f'Playing {yt.title}!', description=yt.author, color=0xc4302b)
-        embed.set_thumbnail(url = thumbnail_url)
-        await ctx.send(embed=embed, delete_after=30)
+        await ctx.send('Now playing: {}'.format(player.title))
 
 
     @commands.command(aliases=['l'])
@@ -116,7 +85,6 @@ class music(commands.Cog):
 
 
     @play.before_invoke
-    @search.before_invoke
     @leave.before_invoke
     async def ensure_voice(self, ctx):
         if ctx.voice_client is None:
